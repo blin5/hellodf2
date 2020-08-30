738fd4024c2bfcf849f79ed301e40f2e2d03f8e0
blin5 <avatargiuliano@hotmail.it>
2020-08-30 23:53:15 +0200

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import tonyImage from './img/tonyPic.jpg';
+import backgroundVideo from './video_back/background.mp4';
+import { FileUpload } from 'primereact/fileupload';
+import { Card } from 'primereact/card';
 import './App.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import Webcam from "react-webcam";
+
+const WebcamComponent = () => <Webcam />;
 
 function App() {
   return (
     <div className="App">
-      <h1>DARK POLO GANG</h1>
-      <h2>TONY EFFE GENERATOR</h2>
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1"
+        }}
+        >
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
       <img src={tonyImage} alt="tony-effe" />
+
+      <div className="Card">
+      <Card title="Upload face" style={{width: '30em'}} >
+        <FileUpload name="demo[]" url="./upload.php" onUpload={onUpload} accept="image/*" maxFileSize={1000000}
+                        emptyTemplate={<p className="p-m-0">Drag and drop files to here to upload.</p>} />
+      </Card>
+      </div>
     </div>
   );
+}
+
+function onUpload() {
+  this.toast.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
 }
 
 export default App;
