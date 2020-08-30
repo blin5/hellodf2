3be993c53ae6e5e8a9256ad894c0247bc37f956a
blin5 <avatargiuliano@hotmail.it>
2020-08-30 17:17:03 +0200

    Updates

---
+++ precache-manifest.17f5a4239c648247c332b4c2127a91ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a743d89f15f83416271632149d8129f5",
+    "url": "/hellodf2/index.html"
+  },
+  {
+    "revision": "11b42eb210080f5cdcbf",
+    "url": "/hellodf2/static/css/main.7928b2f3.chunk.css"
+  },
+  {
+    "revision": "d6633c7e76fe65b23f25",
+    "url": "/hellodf2/static/js/2.a7ab4590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hellodf2/static/js/2.a7ab4590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b42eb210080f5cdcbf",
+    "url": "/hellodf2/static/js/main.93070971.chunk.js"
+  },
+  {
+    "revision": "494de256463b906e27c2",
+    "url": "/hellodf2/static/js/runtime-main.7ebfb35a.js"
+  },
+  {
+    "revision": "c6fc34ab10507b1a6879cbde0aeb0166",
+    "url": "/hellodf2/static/media/tonyPic.c6fc34ab.jpg"
+  }
+]);
